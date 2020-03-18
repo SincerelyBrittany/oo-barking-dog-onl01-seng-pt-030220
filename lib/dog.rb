38bd424d841b15301c35749e3b6710ad85dfd2d5
Brittany Russell <sincerelybr@gmail.com>
2020-03-18 22:35:48 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,13 +5,12 @@ class Dog
     @this_dogs_name = dogs_name
   end
  
-  # def bark
-  #   "woof!"
-  # end
-  
+  def name
+    @this_dogs_name
+  end
 end
  
-# lassie = Dog.new
-# lassie.name = "Lassie"
+lassie = Dog.new
+lassie.name = "Lassie"
  
-# puts lassie.name
+puts lassie.name
